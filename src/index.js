5a99b7db2e0e7b674065eebaebf6df85c036f519
Jimmy Yang <128646419+JYang496@users.noreply.github.com>
2024-02-25 15:11:51 -0500

    update format

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import './index.css';
 import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// const reduxStore = createStore(reducers, applyMiddleware(thunk))
 
 
 root.render(
-    // <Provider store={reduxStore}>
         <App/>
-    // </Provider>
 );
 
