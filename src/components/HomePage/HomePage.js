306e591efc450d287650d9966f801e92c023cfd4
Jimmy Yang <128646419+JYang496@users.noreply.github.com>
2024-02-27 16:33:27 -0500

    hero implement

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,7 +1,14 @@
 import "./HomePage.scss"
+import "../General/General.scss"
 
 export const HomePage = () => {
-    return <>
-        homepage
-    </>
+    return <div className="home-page">
+        <section id="hero" className="d-flex flex-column justify-content-center align-items-center">
+            <div className="hero-container" data-aos="fade-in">
+                <h1>Alex Smith</h1>
+                <p>I'm <span className="typed" data-typed-items="Designer, Developer, Freelancer, Photographer"></span>
+                </p>
+            </div>
+        </section>
+    </div>
 }
