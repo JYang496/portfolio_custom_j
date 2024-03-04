8658d4fb0d573958317b5ae17aa219d8522e3cdb
Jimmy Yang <128646419+JYang496@users.noreply.github.com>
2024-03-04 16:27:42 -0500

    Header implementation & img import

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/app.scss';
+import {Header} from "./Header";
+
+function App() {
+  return (
+    <div className="app">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
