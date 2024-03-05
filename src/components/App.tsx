ec4e05232a76e14684e572869a0b0283d1aa3e1d
Jimmy Yang <128646419+JYang496@users.noreply.github.com>
2024-03-05 15:35:20 -0500

    layout implement

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './styles/app.scss';
 import {Header} from "./Header";
+import {Hero} from "./Hero";
+import {Main} from "./Main";
+import {Footer} from "./Footer";
 
 function App() {
   return (
     <div className="app">
-      <Header/>
+        <Header/>
+        <Hero/>
+        <Main/>
+        <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import './styles/general.scss'
+
+export const Footer = () => {
+  return <>
+      <footer id="footer">
+          <div className="container">
+              <div className="copyright">
+                  &copy; Footer <strong><span>Footer</span></strong>
+              </div>
+          </div>
+      </footer>
+  </>
+}
